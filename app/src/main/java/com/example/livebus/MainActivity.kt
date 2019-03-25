a913eb1b706c3ebee534b4d6658e94a163861a2c
Kimger <kimgerxue@gmail.com>
2019-03-25 17:37:07 +0800

    commit

--- app/src/main/java/com/example/livebus/MainActivity.kt
+++ app/src/main/java/com/example/livebus/MainActivity.kt
@@ -1,26 +1,36 @@
 package com.example.livebus
 
-import LiveBus
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.livebus.event.EventMsg
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var int = 0
-        btn_send.setOnClickListener { LiveBus.default.postSticky("test", EventMsg("测试消息发送成功" + int++)) }
-        btn_observe.setOnClickListener {
-            LiveBus.default.subscribe<EventMsg>(this, "test")
-                .observe(this, Observer { t -> Toast.makeText(this, t.msg, Toast.LENGTH_SHORT).show() })
-        }
+        btn_send.setOnClickListener { LiveBus.default.postSticky(EventMsg("要发送的消息")) }
+        btn_observe.setOnClickListener { LiveBus.default.post("eventKey", "阿U而后覅偶安慰害怕") }
         btn_next.setOnClickListener { startActivity(Intent(this, TestActivity::class.java)) }
+//        LiveBus.getDefault().subscribe<EventMsg>(EventMsg::class.java)
+//            .observe(this, Observer { t -> Toast.makeText(this, t.msg, Toast.LENGTH_SHORT).show() })
+//        btn_next.setOnClickListener { LiveBus.getDefault().post(EventMsg("33333333333333")) }
+
+        LiveBus.default.subscribe(EventMsg::class.java)
+            .observe(this, Observer { t -> Toast.makeText(this, t.msg, Toast.LENGTH_SHORT).show() })
+
+
+        LiveBus.default.subscribe<String>("eventKey")
+            .observe(this, Observer { t -> Toast.makeText(this, t, Toast.LENGTH_SHORT).show() })
+
+
+        LiveBus.default.subscribe(DataBean::class.java)
+            .observe(this, Observer { bean -> })
+
     }
+
 }
